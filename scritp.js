1765e5100f135d1e62be2421f628427fdcdebfb4
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-26 23:40:39 +0100

    add: a new script

---
+++ scritp.js
@@ -0,0 +1,4 @@
+// Arrays are a useful data structure in JavaScript. They allow you to store a collection of data in a single variable. Arrays are created using square brackets, like this:
+var myArray = [1, 2, 3];
+// This creates an array called 'myArray' and adds three values to it: 1, 2, and 3. Each value is known as an "element" of the array. Individual elements can be accessed using an index number, like this:
+var myValue = myArray[0];
